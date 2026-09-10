e84178fe859965c9fd915e6b76fa71667ffe8e07
Victoria Nilsson <niklas@trevligasidor.se>
2026-09-10 14:37:18 +0200

    uppdaterade vite config

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,46 +1,48 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const phasermsg = () => {
     return {
-        name: 'phasermsg',
+        name: "phasermsg",
         buildStart() {
             process.stdout.write(`Building for production...\n`);
         },
         buildEnd() {
-            const line = "---------------------------------------------------------";
+            const line =
+                "---------------------------------------------------------";
             const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
             process.stdout.write(`${line}\n${msg}\n${line}\n`);
 
             process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}
+        },
+    };
+};
 
 export default defineConfig({
-    base: './',
-    plugins: [
-        react(),
-        phasermsg()
-    ],
-    logLevel: 'warning',
+    base: "./",
+    plugins: [react(), phasermsg()],
+    logLevel: "warning",
+    preview: {
+        host: "0.0.0.0",
+        allowedHosts: ["klient-production.up.railway.app"],
+    },
     build: {
         rollupOptions: {
             output: {
                 manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+                    phaser: ["phaser"],
+                },
+            },
         },
-        minify: 'terser',
+        minify: "terser",
         terserOptions: {
             compress: {
-                passes: 2
+                passes: 2,
             },
             mangle: true,
             format: {
-                comments: false
-            }
-        }
-    }
+                comments: false,
+            },
+        },
+    },
 });
